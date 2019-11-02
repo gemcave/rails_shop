1259bba523ee8f5d7171d574469909524947f8da
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-11-02 15:42:02 +0600

    cart,user and cart models

---
+++ spec/models/cart_item_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe CartItem, type: :model do
+	it { should belong_to(:product) }
+	it { should belong_to(:cart) }
+
+	it { should validate_presence_of(:quantity)} 
+	it { should validate_presence_of(:cart_id)} 
+end
